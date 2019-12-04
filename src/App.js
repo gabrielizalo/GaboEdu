a797ea6aaaaa2c2a5aa920e5e965b15bcce85ba7
ghporras@gmail.com <dG26ZeK6tyRP>
2019-12-04 09:00:32 -0500

    feat: added JSXs tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './styles/styles.scss';
 
-const App = () => <h1>Hola Mundo</h1>;
+const App = () => (
+    <>
+        <div className="hello">
+            <h1>Hola mundo</h1>
+            <p>Hola, saludando desde mi componente</p>
+        </div>
+        <div>
+            <h2>Hola mundo son un H2</h2>
+            <p>Hola, saludando desde mi componente, yo soy h2</p>
+            <img src="https://ed.team/static/images/logo-premium.svg" alt=""/>
+        </div>
+    </>
+);
 
 export default App;
