76d2e227302f2915841aebe6f72165b411064d4d
ghporras@gmail.com <dG26ZeK6tyRP>
2019-12-20 14:54:19 -0500

    feat: added state updates with methods

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,10 +3,26 @@ import React, { Component } from "react";
 class Form extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             nombre: "",
             correo: ""
         };
+
+        this.changeName = this.changeName.bind(this)
+        this.changeEmail = this.changeEmail.bind(this)
+    }
+
+    changeName(e) {
+        this.setState( {
+            nombre: e.target.value
+        } )
+    }
+
+    changeEmail(e) {
+        this.setState( {
+            correo: e.target.value
+        } )
     }
 
     render() {
@@ -17,11 +33,11 @@ class Form extends Component {
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
                             <label htmlFor="">Nombre completo</label>
-                            <input type="text" onChange={ e => this.setState( { nombre: e.target.value } ) } />
+                            <input type="text" onChange={ this.changeName } />
                         </div>
                         <div className="form__item">
                             <label htmlFor="">Email</label>
-                            <input type="email"  onChange={ e => this.setState( { correo: e.target.value } ) } />
+                            <input type="email"  onChange={ this.changeEmail } />
                         </div>
                     </div>
                 </form>
