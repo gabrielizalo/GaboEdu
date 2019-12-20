9fa8c4a2c5ca737d717b511c5f91fa33359c9bd3
Gabriel Porras <ghporras@gmail.com>
2019-12-19 19:20:10 -0500

    feat: added an state

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 class Form extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            nombre: "",
+            correo: ""
+        };
     }
 
     render() {
@@ -10,18 +14,19 @@ class Form extends Component {
             <div className="ed-grid">
                 <h1>Formulario</h1>
                 <form action="">
-                    <div className="form__item">
-                        <label htmlFor="">Nombre completo</label>
-                        <input type="text" />
-                    </div>
-                    <div className="form__item">
-                        <label htmlFor="">Email</label>
-                        <input type="email" />
-                    </div>
-                    <div className="form__item">
-                        <input type="submit" value="Enviar" className="button full"/>
+                    <div className="ed-grid m-grid-2">
+                        <div className="form__item">
+                            <label htmlFor="">Nombre completo</label>
+                            <input type="text" />
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="">Email</label>
+                            <input type="email" />
+                        </div>
                     </div>
                 </form>
+                <h2>{`Hola ${this.state.nombre}`}</h2>
+                <span>{`Tu correo es: ${this.state.correo}`}</span>
             </div>
         );
     }
