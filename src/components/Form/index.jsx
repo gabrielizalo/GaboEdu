797c94d55e210df86e36858ae29d9f22346ef43a
Gabriel Porras <ghporras@gmail.com>
2019-12-22 15:12:56 -0500

    feat: added missing methods

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,6 +7,16 @@ class Form extends Component {
             nombre: "",
             correo: ""
         };
+        this.changeName = this.changeName.bind(this);
+        this.changeEmail = this.changeEmail.bind(this);
+    }
+
+    changeName(e) {
+        this.setState({ nombre: e.target.value });
+    }
+
+    changeEmail(e) {
+        this.setState({ correo: e.target.value });
     }
 
     componentDidMount() {
@@ -22,11 +32,11 @@ class Form extends Component {
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
                             <label htmlFor="">Nombre completo</label>
-                            <input type="text" />
+                            <input type="text" onChange={this.changeName} />
                         </div>
                         <div className="form__item">
                             <label htmlFor="">Email</label>
-                            <input type="email" />
+                            <input type="email" onChange={this.changeEmail} />
                         </div>
                     </div>
                 </form>
