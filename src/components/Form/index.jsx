a2e9483a3fd16b31e473061b63873758c59390e3
Gabriel Porras <ghporras@gmail.com>
2019-12-22 15:35:05 -0500

    feat: added another life cycle method

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,10 +5,12 @@ class Form extends Component {
         super(props);
         this.state = {
             nombre: "",
-            correo: ""
+            correo: "",
+            fecha: new Date()
         };
         this.changeName = this.changeName.bind(this);
         this.changeEmail = this.changeEmail.bind(this);
+        this.changeDate = this.changeDate.bind(this);
     }
 
     changeName(e) {
@@ -19,9 +21,17 @@ class Form extends Component {
         this.setState({ correo: e.target.value });
     }
 
+    changeDate() {
+        this.setState({ fecha: new Date() });
+    }
+
     componentDidMount() {
         let elemento = document.getElementById("elemento");
         console.log(elemento);
+
+        this.intervalDate = setInterval(() => { this.changeDate() }, 1000);
+
+        console.log(new Date())
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -30,10 +40,15 @@ class Form extends Component {
         console.log("-------------------");
     }
 
+    componentWillUnmount() {
+        clearInterval(this.intervalDate)
+    }
+
     render() {
         return (
             <div className="ed-grid">
                 <h1>Formulario {this.props.name}</h1>
+                <h4>{this.state.fecha / 1000}</h4>
                 <form action="" id="elemento">
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
