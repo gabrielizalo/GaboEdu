c79155734eca0d6ce2ee414f96f26f74e6cf4466
Gabriel Porras <ghporras@gmail.com>
2020-01-02 19:03:16 -0500

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Banner from "./components/Banner";
 import Form from "./components/Form";
 import CourseGrid from "./components/CourseGrid";
+
 import "./styles/styles.scss";
 
 const App = () => (
-    <>
-        <Banner />
-        <Form name={"EDTeam"} />
-        <CourseGrid />
-    </>
+    <Router>
+        <Route path="/" exact component={Banner} />
+        <Route path="/cursos" exact component={CourseGrid} />
+        <Route
+            path="/contacto"
+            exact
+            component={() => <Form name={"EDTeam"} />}
+        />
+        <Route
+            component={() => (
+                <div className="ed-grid">
+                    <h1>Error 404</h1>
+                    <span>Página no encontrada</span>
+                </div>
+            )}
+        />
+    </Router>
 );
 
 export default App;
