976083b1d5e11f3db2dae13249dcabbf54f6f9ad
Gabriel Porras <ghporras@gmail.com>
2020-01-02 19:15:16 -0500

    feat: added switch component to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Banner from "./components/Banner";
 import Form from "./components/Form";
@@ -9,21 +9,23 @@ import "./styles/styles.scss";
 
 const App = () => (
     <Router>
-        <Route path="/" exact component={Banner} />
-        <Route path="/cursos" exact component={CourseGrid} />
-        <Route
-            path="/contacto"
-            exact
-            component={() => <Form name={"EDTeam"} />}
-        />
-        <Route
-            component={() => (
-                <div className="ed-grid">
-                    <h1>Error 404</h1>
-                    <span>Página no encontrada</span>
-                </div>
-            )}
-        />
+        <Switch>
+            <Route path="/" exact component={Banner} />
+            <Route path="/cursos" exact component={CourseGrid} />
+            <Route
+                path="/contacto"
+                exact
+                component={() => <Form name={"EDTeam"} />}
+            />
+            <Route
+                component={() => (
+                    <div className="ed-grid">
+                        <h1>Error 404</h1>
+                        <span>Página no encontrada</span>
+                    </div>
+                )}
+            />
+        </Switch>
     </Router>
 );
 
